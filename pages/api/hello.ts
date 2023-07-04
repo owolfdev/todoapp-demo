8380ec14f95c751adde7a01a79febf7f483a0103
owolfdev <owolfdev@gmail.com>
2023-07-04 07:45:04 +0700

    post

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,46 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+
+// type Data = {
+//   name: string
+// }
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+import { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  email: string;
+  project: string;
+};
+
+type ErrorResponse = {
+  error: string;
+};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | ErrorResponse>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "POST") {
+    const { email, project } = req.body;
+
+    console.log("Here's the email:", email);
+    console.log("Here's the project:", project);
+
+    const data: Data = {
+      email,
+      project,
+    };
+
+    res.status(200).json(data);
+  } else {
+    const errorResponse: ErrorResponse = { error: "Method Not Allowed" };
+    res.status(405).json(errorResponse);
+  }
 }
